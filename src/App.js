a6fef8e4680f1ec42772f46064c4e30f40419ebb
FCanfran <ferchomartin9@gmail.com>
2021-03-20 14:11:47 +0100

    Init

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import React, { useState } from 'react';
+import {useRoutes} from 'hookrouter';
+//import Routes from './Routes';
 
 import Menu from './Menu';
 import Login from './Login';
+import Prueba from "./Prueba";
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   const [logged, setLogged] = useState(false);
+  const routes = {
+    "/": () => {logged ? <Menu /> : <Login onLogin={() => setLogged(true)} />},
+    "/login": () => <Login />,
+    "/prueba": () => <Prueba />,
+    "/menu": () => <Menu />
+  };
+  
+  const routeResult = useRoutes(routes);
 
-  return (
-    <div className="App">
-      {logged ? <Menu /> : <Login onLogin={() => setLogged(true)} />}
-    </div>
-  );
+  return (routeResult);
 }
 
 export default App;
--- src/Menu.js
+++ src/Menu.js
@@ -1,32 +1,37 @@
+import { setLinkProps } from 'hookrouter';
 import React from 'react';
 import {Container, Row, Col, Button, Image} from 'react-bootstrap';
 
 import logo from './logo.svg';
 
-function Menu() {
-  return (
-    <Container className="app-container">
-      <Row>
-        <Col md={4}>Perfil</Col>
-        <Col md={{span:4, offset: 4}}>Tienda</Col>
-      </Row>
+class Menu extends React.Component {
+  
+  render() {
+    return (
+        <Container className="app-container">
+          <Row>
+            <Col md={4}>Perfil</Col>
+            <Col md={{span:4, offset: 4}}>Tienda</Col>
+          </Row>
 
-      <Row className="align-items-center">
-        <Col>
-          <Image className="py-5" src={logo} alt="logo" fluid/>
-        </Col>
-        <Col>
-          <h1>GATOVID</h1>
-        </Col>
-      </Row>
+          <Row className="align-items-center">
+            <Col>
+              <Image className="py-5" src={logo} alt="logo" fluid/>
+            </Col>
+            <Col>
+              <h1>GATOVID</h1>
+            </Col>
+          </Row>
 
-      <Row>
-        <Col md={4}><Button>Crear partida privada</Button></Col>
-        <Col md={4}><Button>Unirse partida privada</Button></Col>
-        <Col md={4}><Button>Unirse partida pública</Button></Col>
-      </Row>
-    </Container>
-  );
-}
+          <Row>
+            <Col md={4}><Button onClick={() => {window.location.href='/prueba';}}>Crear partida privada</Button></Col>
+            <Col md={4}><Button>Unirse partida privada</Button></Col>
+            <Col md={4}><Button>Unirse partida pública</Button></Col>
+          </Row>
+
+        </Container>
+      );
+    }
+};
 
 export default Menu;
