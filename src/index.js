55cbe0af8fe90e401148863e44533f9ff5f4899b
xzebra <zebrv.apps@gmail.com>
2021-03-21 23:49:54 +0100

    remove unused import

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { PopupboxManager, PopupboxContainer } from "react-popupbox";
+import { PopupboxContainer } from "react-popupbox";
 
 import './index.css';
 import "react-popupbox/dist/react-popupbox.css"
