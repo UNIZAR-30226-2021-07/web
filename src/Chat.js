3b2d7f4bb76512cdb6f6a05214a664d86e078964
José Marín Díez <josemarindiez@gmail.com>
2021-03-22 13:17:25 +0100

    Componente chat

--- src/Chat.js
+++ src/Chat.js
@@ -1,34 +1,18 @@
 import React from 'react';
-import { Container, Button, Row, Col, InputGroup } from 'react-bootstrap';
 
-function Mensaje({userid, text}) {
-    return(
-        <Row className="align-items-center">
-            <Col md={2}>{userid}: </Col>
-            <Col md={10}>{text}</Col>
-        </Row>
-    )
-}
+import { Container, Button, Row} from 'react-bootstrap';
 
-function Chat(){
+function Chat({messages}){
     return(
         <Container>
             <Container>
-                <h1>Chat de partida</h1>
+                <h4>Chat de partida</h4>
             </Container>
+            <Container>{messages}</Container>
             <Container>
-                <Mensaje userid="Nombre 1"text="Mensaje 1"/>
-                <Mensaje userid="Nombre 2" text="Mensaje 2"/>
-                <Mensaje userid="Nombre 3" text="Mensaje 4"/>
-            </Container>
-            <Container>
-                <Row>
-                    <Col>
-                    <input type="email" class="form-control" border-color="red" placeholder="Escribir Mensaje"></input>
-                    </Col>
-                    <Col>
-                        <Button>Send</Button>
-                    </Col>
+                <Row >
+                    <input placeholder="Escribir Mensaje"></input>
+                    <Button>Send</Button>
                 </Row>
             </Container>          
         </Container>
---
+++ src/Chat.test.js
@@ -0,0 +1,26 @@
+import { render, screen } from '@testing-library/react';
+import Chat from './Chat';
+import Message from '.Message'
+
+test('renders title', () => {
+    render(<Chat />);
+    const titleElement = screen.getByText(/Chat de partida/i);
+    expect(titleElement).toBeInTheDocument();
+});
+
+test('renders messages', () => {
+    const messages = [     
+        <div>     
+          <Message userid="Nombre 1"text="Mensaje 1"/>
+          <Message userid="Nombre 2" text="Mensaje 2"/>
+          <Message userid="Nombre 3" text="Mensaje 3"/>
+        </div>      
+    ];
+    render( <Chat messages={messages}/>);
+    var usernameElement = screen.getByText("Nombre 1: Mensaje 1");
+    expect(usernameElement).toBeInTheDocument();
+    usernameElement = screen.getByText("Nombre 2: Mensaje 2");
+    expect(usernameElement).toBeInTheDocument();
+    usernameElement = screen.getByText("Nombre 3: Mensaje 3");
+    expect(usernameElement).toBeInTheDocument();
+});
