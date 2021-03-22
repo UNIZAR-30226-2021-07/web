1558c642d7ff0b96263304075fb69add85e30d74
Mario Ortiz Manero <marioortizmanero@gmail.com>
2021-03-22 19:21:23 +0100

    fix lints

--- src/Perfil.js
+++ src/Perfil.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Row, Col, Card, Button, Image } from "react-bootstrap";
+import { Container, Row, Col, Card, Button } from "react-bootstrap";
 
 import logo from "./logo.svg";
 
