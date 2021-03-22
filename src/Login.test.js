6130d5a57b91f9de0ab86d56f13197ab0cdc9e0c
José Marín Díez <josemarindiez@gmail.com>
2021-03-22 17:02:32 +0100

    Añadida libreria react a tests

--- src/Login.test.js
+++ src/Login.test.js
@@ -1,4 +1,5 @@
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import React from "react";
 import Login from "./Login";
 
 test("renders submit button", () => {
--- src/SignUp.test.js
+++ src/SignUp.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import React from "react";
 import SignUp from "./SignUp";
 
 test("renders title", () => {
