4f5d3ed47ec080cd88c9521a383d0d365b4c2a0c
FCanfran <ferchomartin9@gmail.com>
2021-03-23 10:56:07 +0100

    tests

--- src/EditProfile.test.js
+++ src/EditProfile.test.js
@@ -2,33 +2,23 @@ import { render, screen } from "@testing-library/react";
 import React from "react";
 import EditProfile from "./EditProfile";
 
-test("renders username", () => {
+test("renders EditProfile", () => {
   var username = "Juan Carlos";
   var email = "juanCarlos@gmail.com";
-  var games = "15";
-  var wins = "7";
-  var losts = "8";
-  var timePlayed = "145";
+  var boardColor = "#05ff82";
+
   render(
-    <EditProfile
-      username={username}
+    <EditProfile username={username}
       email={email}
-      games={games}
-      wins={wins}
-      losts={losts}
-      timePlayed={timePlayed}
-    />
+      boardColor={boardColor}/>
   );
+
   const usernameElement = screen.getByText(username);
   expect(usernameElement).toBeInTheDocument();
+
   const emailElement = screen.getByText(email);
   expect(emailElement).toBeInTheDocument();
-  const gamesElement = screen.getByText(games);
-  expect(gamesElement).toBeInTheDocument();
-  const winsElement = screen.getByText(wins);
-  expect(winsElement).toBeInTheDocument();
-  const lostsElement = screen.getByText(losts);
-  expect(lostsElement).toBeInTheDocument();
-  const timePlayedElement = screen.getByText(timePlayed + " min");
-  expect(timePlayedElement).toBeInTheDocument();
+
+  const boardColorElement = screen.getByText(boardColor);
+  expect(boardColorElement).toBeInTheDocument();
 });
