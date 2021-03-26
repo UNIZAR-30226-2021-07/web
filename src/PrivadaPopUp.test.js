69519e2bfa7a0f6ac2a41972784544a3399ae189
José Marín Díez <josemarindiez@gmail.com>
2021-03-26 16:59:11 +0100

    test popup partida privada

--- src/PrivadaPopUp.test.js
+++ src/PrivadaPopUp.test.js
@@ -1,3 +1,21 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import PrivadaPopUp from "./PrivadaPopUp";
+import PrivatePopUp from "./PrivatePopUp";
+
+test("renders title", () => {
+  render(<PrivatePopUp/>);
+  const titleElement = screen.getByText(/Unirse a partida/i);
+  expect(titleElement).toBeInTheDocument();
+});
+
+test("renders confirm button", () => {
+  render(<PrivatePopUp />);
+  const buttonElement = screen.getByText(/CONFIRMAR/i);
+  expect(buttonElement).toBeInTheDocument()
+});
+
+test("check input is correct", () => {
+  render(<PrivatePopUp />);
+  const inputElement = screen.getByText(/código/i);
+  expect(inputElement).toBeInTheDocument()
+});
