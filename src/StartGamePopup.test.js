2b1e9ef1f49eb92cb837734820d6fb502dd68e22
José Marín Díez <josemarindiez@gmail.com>
2021-03-26 20:28:29 +0100

    test dont run

---
+++ src/StartGamePopup.test.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import StartGamePopup from "./StartGamePopup";
+
+test("renders title", () => {
+  render(<StartGamePopup ready="4" total="6"/>);
+  const titleElement = screen.getByText(/¿Empezar partida\?/i);
+  expect(titleElement).toBeInTheDocument();
+});
+
+test("renders start button", () => {
+  render(<StartGamePopup ready="4" total="6"/>);
+  const buttonElement = screen.getByText("Empezar Partida");
+  expect(buttonElement).toBeInTheDocument()
+});
+
+test("check counter", () => {
+  render(<StartGamePopup ready="4" total="6"/>);
+  const counterElement = screen.getByText(/4\/6 gaticos preparados/i);
+  expect(counterElement).toBeInTheDocument()
+}); 
