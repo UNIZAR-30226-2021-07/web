9fba6d4846d1667b55a9c54e722e261935b9952b
José Marín Díez <josemarindiez@gmail.com>
2021-03-26 16:45:08 +0100

    Popup terminado

--- src/PrivatePopUp.js
+++ src/PrivatePopUp.js
@@ -2,35 +2,42 @@ import React from "react";
 import { PopupboxManager } from "react-popupbox";
 import { Container, Row, Col, Button } from "react-bootstrap";
 
-export default function PrivatePopup({ title, children }) {
+export default function PrivatePopup({ children }) {
   return (
     <Container>
-      <Row>
-        <div className="d-flex justify-content-between col-12">
-          <div>
-            <h2 className="popup-title">{title}</h2>
-          </div>
-          {/* Botón de cierre si se necesita (no está en el diseño) */}
-          <div className="d-flex">
-            <Button
-              className="close"
-              aria-label="Close"
-              onClick={PopupboxManager.close}
-            >
-              <span aria-hidden="true">&times;</span>
-            </Button>
-          </div>
-          {/* Fin botón de cierre */}
-        </div>
-        <Col>{children}</Col>
+      <Row className="d-flex justify-content-center">
+        <div>Tick Icon</div>
+        <h2 className="popup-title">Unirse a partida</h2>
       </Row>
+      <Row className="d-flex justify-content-center">
+        <p>
+          Introduce el código de partida y <br></br>
+          comienza a jugar con tus amigos.
+        </p>
+      </Row>
+      <Row className="d-flex justify-content-between">
+        <input className= "code-box"
+          type="text"
+          size = "8"
+          minLength="4"
+          maxLength="8"
+          placeholder="Código"
+        ></input>
+        <Button
+          className="btn btn-primary"
+          onClick={PopupboxManager.close}
+        >
+          CONFIRMAR
+        </Button>
+      </Row>
+      <Col>{children}</Col>
     </Container>
   );
 }
 
 // For test purposes only
-export function openPrivatePopup(title) {
-  const content = <PrivatePopup title={title} />;
+export function openPrivatePopup() {
+  const content = <PrivatePopup />;
   PopupboxManager.open({
     content,
     config: {
