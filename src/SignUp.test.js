1cc26b955865d7f3cd6f494765bcfbb94935ccbd
xzebra <zebrv.apps@gmail.com>
2021-03-30 14:37:16 +0200

    use router in signup test

--- src/SignUp.test.js
+++ src/SignUp.test.js
@@ -1,21 +1,34 @@
-import { render, screen } from "@testing-library/react";
 import React from "react";
+import { render, screen } from "@testing-library/react";
+import { BrowserRouter as Router } from "react-router-dom";
 import SignUp from "./SignUp";
 
 test("renders title", () => {
-  render(<SignUp />);
+  render(
+    <Router>
+      <SignUp />
+    </Router>
+  );
   const titleElement = screen.getByText(/Iniciar Sesión/i);
   expect(titleElement).toBeInTheDocument();
 });
 
 test("renders iniciar sesion button", () => {
-  render(<SignUp />);
+  render(
+    <Router>
+      <SignUp />
+    </Router>
+  );
   const buttonElement = screen.getByText(/Iniciar Sesión/i);
   expect(buttonElement).toBeInTheDocument();
 });
 
 test("renders crear cuenta button", () => {
-  render(<SignUp />);
+  render(
+    <Router>
+      <SignUp />
+    </Router>
+  );
   const buttonElement = screen.getByText(/Crear una cuenta/i);
   expect(buttonElement).toBeInTheDocument();
 });
