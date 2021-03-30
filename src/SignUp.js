4bf2e7704daefa96304b224b2f33c426814221c4
José Marín Díez <josemarindiez@gmail.com>
2021-03-30 16:15:58 +0200

    estilo

--- src/SignUp.js
+++ src/SignUp.js
@@ -4,35 +4,40 @@ import { Link } from "react-router-dom";
 
 function SignUp() {
   return (
-    <Container>
+    <Container className="app-container">
       <Card>
         <Card.Body>
-          <Card.Title>Crear una cuenta</Card.Title>
+          <Row className="justify-content-center">
+            <Card.Title className="primary-title text-align-center">
+              Crear una cuenta
+            </Card.Title>
+          </Row>
+          <Form className="justify-content-center">
+            <Form.Group controlId="formBasicUser">
+              <Form.Label>Nombre de Usuario</Form.Label>
+              <Form.Control type="text" />
+            </Form.Group>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Email</Form.Label>
+              <Form.Control type="email" />
+            </Form.Group>
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Contraseña</Form.Label>
+              <Form.Control type="password" />
+            </Form.Group>
+            <Link to="/menu">
+              <Button className="primary-button">REGISTRARSE</Button>
+            </Link>
+          </Form>
         </Card.Body>
-        <Form>
-          <Form.Group controlId="formBasicUser">
-            <Form.Label>Nombre de Usuario</Form.Label>
-            <Form.Control type="text" />
-          </Form.Group>
-          <Form.Group controlId="formBasicEmail">
-            <Form.Label>Email</Form.Label>
-            <Form.Control type="email" />
-          </Form.Group>
-          <Form.Group controlId="formBasicPassword">
-            <Form.Label>Contraseña</Form.Label>
-            <Form.Control type="password" />
-          </Form.Group>
-          <Button variant="primary" type="submit">
-            REGISTRARSE
-          </Button>
-        </Form>
       </Card>
-
       <Container>
-        <Row className="align-items-center">¿Ya tienes cuenta?</Row>
-        <Row className="align-items-center">
+        <Row className="justify-content-center secondary-title">
+          ¿Ya tienes cuenta?
+        </Row>
+        <Row className="justify-content-center">
           <Link to="/login">
-            <Button>INICIAR SESIÓN</Button>
+            <Button  className="secondary-button">INICIAR SESIÓN</Button>
           </Link>
         </Row>
       </Container>
