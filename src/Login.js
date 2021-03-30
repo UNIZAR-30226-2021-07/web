31f3f827fccf9ec1dddfae426d25261c8f215969
José Marín Díez <josemarindiez@gmail.com>
2021-03-30 10:58:16 +0200

    centrado cartas con margen

--- src/Login.js
+++ src/Login.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 function Login({ onLogin }) {
   return (
-    <Container className="app-container centered">
+    <Container className="app-container">
       <Card>
         <Card.Body>
           <Row className="justify-content-center">
@@ -33,15 +33,12 @@ function Login({ onLogin }) {
                 variant="primary"
                 type="submit"
               >
-                {" "}
                 INICIAR SESIÓN
               </Button>
             </Link>
           </Form>
         </Card.Body>
       </Card>
-      <br></br>
-      <br></br>
       <Container>
         <Row className="justify-content-center secondary-title">
           ¿Nuevo en Gatovid?
