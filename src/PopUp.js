608c57256d6fc98afcd0d4c4b8d3878f8088e71a
Mario Ortiz Manero <marioortizmanero@gmail.com>
2021-03-31 17:33:42 +0200

    full fix for popup, much easier w/ bootstrap

--- src/PopUp.js
+++ src/PopUp.js
@@ -1,18 +1,17 @@
 import React from "react";
 import { PopupboxManager } from "react-popupbox";
-import { Container, Col, Image } from "react-bootstrap";
+import { Container, Row, Image } from "react-bootstrap";
 
 export default function Popup({ title, icon, children }) {
   return (
     <Container>
-      <div>
-        <div className="d-flex justify-content-between align-items-center col-12 mb-2">
-          <Image src={icon} className="mr-2" style={{"height": "30px"}} fluid>
-          </Image>
-          <h2 className="popup-title mb-0">{title}</h2>
-        </div>
-      </div>
-      <Col>{children}</Col>
+      <Row className="justify-content-center mb-2 align-items-center">
+        <Image src={icon} className="mr-2" style={{"height": "30px"}} fluid></Image>
+        <h2 className="popup-title mb-0">{title}</h2>
+      </Row>
+      <Row>
+          {children}
+      </Row>
     </Container>
   );
 }
