42c87f4222208a79cb34b96de03de9ca2ec94257
FCanfran <ferchomartin9@gmail.com>
2021-03-31 11:18:07 +0200

    navegacion

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import { Route, Switch, Redirect } from "react-router-dom";
 import Menu from "./Menu";
 import Match from "./Match";
 import Login from "./Login";
+import Profile from "./Profile";
+import EditProfile from "./EditProfile";
+import SignUp from "./SignUp";
+import Shop from "./Shop"
+import Help from "./Help"
 
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -18,9 +23,21 @@ function App() {
           <Login onLogin={() => setLogged(true)} />
         </Route>
 
+        <Route path="/signup">
+          <SignUp />
+        </Route>
+
         <ProtectedRoute path="/home" loggedIn={logged} component={Menu} />
 
         <ProtectedRoute path="/match" loggedIn={logged} component={Match} />
+
+        <ProtectedRoute path="/profile" loggedIn={logged} component={Profile} />
+
+        <ProtectedRoute path="/editProfile" loggedIn={logged} component={EditProfile} />
+
+        <ProtectedRoute path="/shop" loggedIn={logged} component={Shop} />
+
+        <ProtectedRoute path="/help" loggedIn={logged} component={Help} />
 
         <Route path="/">
           {logged ? <Redirect to="/home" /> : <Redirect to="/login" />}
@@ -42,3 +59,4 @@ const ProtectedRoute = ({ component: Component, loggedIn, ...rest }) => {
 };
 
 export default App;
+
