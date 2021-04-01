c243ce3c8be9eda6ea572e874377a0955fe8eee6
José Marín Díez <josemarindiez@gmail.com>
2021-04-01 11:26:28 +0200

    test

--- src/Login.test.js
+++ src/Login.test.js
@@ -3,11 +3,22 @@ import { render, screen } from "@testing-library/react";
 import { BrowserRouter as Router } from "react-router-dom";
 import Login from "./Login";
 
+/*
 test("renders submit button", () => {
   render(
     <Router>
       <Login />
     </Router>
   );
-  expect(screen.getByText("Iniciar Sesión")).toBeInTheDocument();
+  expect(screen.getAllByText(/Iniciar Sesión/i)).toBeInTheDocument();
+});*/
+
+test("renders crear cuenta button", () => {
+  render(
+    <Router>
+      <Login />
+    </Router>
+  );
+  const buttonElement = screen.getByText(/Crear una cuenta/i);
+  expect(buttonElement).toBeInTheDocument();
 });
--- src/SignUp.test.js
+++ src/SignUp.test.js
@@ -3,33 +3,32 @@ import { render, screen } from "@testing-library/react";
 import { BrowserRouter as Router } from "react-router-dom";
 import SignUp from "./SignUp";
 
-/*
 test("renders title", () => {
   render(
     <Router>
       <SignUp />
     </Router>
   );
-  const titleElement = screen.getByText(/Iniciar Sesión/i);
+  const titleElement = screen.getByText(/Crear una cuenta/i);
   expect(titleElement).toBeInTheDocument();
 });
-
+/*
 test("renders iniciar sesion button", () => {
   render(
     <Router>
       <SignUp />
     </Router>
   );
-  const buttonElement = screen.getByText(/Iniciar Sesión/i);
+  const buttonElement = screen.getByText(/Iniciar sesion/i);
   expect(buttonElement).toBeInTheDocument();
 });*/
 
-test("renders crear cuenta button", () => {
+test("renders registrarse button", () => {
   render(
     <Router>
       <SignUp />
     </Router>
   );
-  const buttonElement = screen.getByText(/Crear una cuenta/i);
+  const buttonElement = screen.getByText(/Registrarse/i);
   expect(buttonElement).toBeInTheDocument();
 });
