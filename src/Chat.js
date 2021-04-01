83553d66951815d5342e7a086216c4916d0078e4
José Marín Díez <josemarindiez@gmail.com>
2021-04-01 21:14:10 +0200

    caja enviar

--- src/Chat.js
+++ src/Chat.js
@@ -1,23 +1,34 @@
 import React from "react";
-import { Container, Button, Row } from "react-bootstrap";
+import { Button, Image, Container, Row } from "react-bootstrap";
+
+import send from "./assets/common/icons/send.svg";
 
 import MessageList from "./MessageList";
 
 function Chat({ messages }) {
   return (
-    <Container>
-      <Container>
+    <Container id="chat" className="px-0 justify-content-space-between">
+      <Row
+        id="chat-header"
+        className="justify-content-center align-items-center"
+      >
         <h4>Chat de partida</h4>
-      </Container>
-      <Container>
+      </Row>
+      <Row className="p-2">
         <MessageList messages={messages} />
-      </Container>
-      <Container>
-        <Row>
-          <input placeholder="Escribir Mensaje"></input>
-          <Button>Send</Button>
-        </Row>
-      </Container>
+      </Row>
+      <Row className="input-group mt-auto ">
+        <input
+          type="text"
+          className="input-chat form-control"
+          placeholder="Escribir Mensaje"
+        ></input>
+        <div className="input-group-append">
+          <Button className="send-button">
+            <Image src={send} style={{ height: "20px" }} />
+          </Button>
+        </div>
+      </Row>
     </Container>
   );
 }
--- src/Match.js
+++ src/Match.js
@@ -4,17 +4,36 @@ import { Link } from "react-router-dom";
 
 import Chat from "./Chat";
 
+//Solo para test
+const messages = [
+  {
+    userid: "Nombre 1",
+    text: "Mensaje 1",
+  },
+  {
+    userid: "Nombre 2",
+    text: "Mensaje 2",
+  },
+  {
+    userid: "Nombre 3",
+    text: "Mensaje 3",
+  },
+];
+
 function Match() {
   return (
-    <Container className="app-container" id="match">
+    <Container
+      className="app-container"
+      id="match"
+    >
       <Row className="align-content-center">
         <Col>
-        <Link to="/">
-          <Button>Salir de la partida</Button>
-        </Link>
+          <Link to="/">
+            <Button>Salir de la partida</Button>
+          </Link>
         </Col>
-        <Col>
-          <Chat/>
+        <Col className="md-3">
+          <Chat messages={messages} />
         </Col>
       </Row>
     </Container>
