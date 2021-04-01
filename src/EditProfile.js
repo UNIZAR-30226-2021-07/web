136505075866257fa153cf190a8f1b023523f02b
FCanfran <ferchomartin9@gmail.com>
2021-04-01 12:39:08 +0200

    pre-merge

--- src/EditProfile.js
+++ src/EditProfile.js
@@ -18,7 +18,7 @@ import { renderDeleteAccountPopup } from "./DeleteAccountPopup";
 
 function EditProfile({ email, username, image, boardType }) {
   return (
-    <Container className="app-container">
+    <Container id="editProfile" className="app-container">
       <Card>
         <Card.Body>
             <Row className="align-items-center justify-content-center">
@@ -29,10 +29,10 @@ function EditProfile({ email, username, image, boardType }) {
                     </Card.Title>
                   </Row>
                   <Row className="align-items-right justify-content-right">
-                  <Button variant="outline-info" size="sm">
-                    <Image fluid src={camera}></Image>
-                  </Button>
-                </Row>
+                    <Button variant="outline-info" size="sm">
+                      <Image fluid src={camera}></Image>
+                    </Button>
+                  </Row>
                   <Row className="align-items-center justify-content-center mb-2">
                       <Image src={image} className="user-profile-image" roundedCircle thumbnail ></Image>
                   </Row>
@@ -42,52 +42,53 @@ function EditProfile({ email, username, image, boardType }) {
                   <Row className="align-items-center justify-content-center mb-2">
                     <Card.Text>{email}</Card.Text>
                   </Row>
-              <Card.Body>
-                <Row className="align-items-center justify-content-center">
-                  <Button className onClick={renderDeleteAccountPopup}>
-                    Eliminar Cuenta
-                  </Button>
-                </Row>
-              </Card.Body>
-          </Col>
-          <Col>
-            <Card.Body>
-              <Form>
-                <Card.Text className="text-muted">
-                  Editar Configuración de Usuario
-                </Card.Text>
-                <Form.Group controlId="formBasicUser">
-                  <Form.Label>Nombre de Usuario</Form.Label>
-                  <Form.Control type="text" placeholder="Nuevo nombre" />
-                </Form.Group>
-                <Form.Group controlId="formBasicPassword">
-                  <Form.Label>Cambiar contraseña</Form.Label>
-                  <Form.Control
-                    type="password"
-                    placeholder="Nueva contraseña"
-                  />
-                </Form.Group>
-                <Form.Group controlId="formBasicBoardType">
-                  <Form.Row>
-                    <Col xs={8}>
-                      <Form.Label>Cambiar Tablero</Form.Label>
-                    </Col>
-                    <Col>
-                      <Button variant="outline-info" size="sm">
-                        <Image fluid src={boardType}></Image>
+                  <Card.Body>
+                    <Row className="align-items-center justify-content-center">
+                      <Button className="alert-button" onClick={renderDeleteAccountPopup}>
+                        Eliminar Cuenta
                       </Button>
-                    </Col>
-                  </Form.Row>
-                </Form.Group>
-              </Form>
-            </Card.Body>
-        </Col>
-        <Card.Body>
-          <Row className="align-items-center justify-content-center">
-            <Button>GUARDAR</Button>
-          </Row>
-        </Card.Body>
-        </Row>
+                    </Row>
+                  </Card.Body>
+               </Col>
+                <Col>
+                  <Card.Body>
+                    <Form>
+                      <Card.Text className="text-muted">
+                        Editar Configuración de Usuario
+                      </Card.Text>
+                      <Form.Group controlId="formBasicUser">
+                        <Form.Label>Nombre de Usuario</Form.Label>
+                        <Form.Control type="text" placeholder="Nuevo nombre" />
+                      </Form.Group>
+                      <Form.Group controlId="formBasicPassword">
+                        <Form.Label>Cambiar contraseña</Form.Label>
+                        <Form.Control
+                          type="password"
+                          placeholder="Nueva contraseña"
+                        />
+                      </Form.Group>
+                      <Form.Group controlId="formBasicBoardType">
+                        <Form.Row>
+                          <Col xs={8}>
+                            <Form.Label>Cambiar Tablero</Form.Label>
+                          </Col>
+                          <Col>
+                              <Image id="imgCambioTablero" style={{"height": "40px", "width": "40px", "border": "1px solid",
+                                               "box-shadow": "0px 4px 4px rgba(0, 0, 0, 0.25)"}} 
+                                      rounded src={boardType}>
+                              </Image>
+                          </Col>
+                        </Form.Row>
+                      </Form.Group>
+                    </Form>
+                  </Card.Body>
+                  <Card.Body>
+                    <Row className="align-items-center justify-content-center">
+                      <Button className="primary-button">GUARDAR</Button>
+                    </Row>
+                  </Card.Body>
+              </Col>
+            </Row>
         </Card.Body>
       </Card>
     </Container>
