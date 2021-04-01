eb234d3ddde64373417dbe177cbe276a265575f6
José Marín Díez <josemarindiez@gmail.com>
2021-04-01 10:35:29 +0200

    X para cerrar en popup generico

--- src/PopUp.js
+++ src/PopUp.js
@@ -1,13 +1,27 @@
 import React from "react";
 import { PopupboxManager } from "react-popupbox";
-import { Container, Row, Image } from "react-bootstrap";
+import { Container, Row, Image, Button } from "react-bootstrap";
 
-export default function Popup({ title, icon, children }) {
+export default function Popup({ title, icon, close, children }) {
   return (
     <Container>
       <Row className="justify-content-center mb-2 align-items-center">
-        <Image src={icon} className="mr-2" style={{"height": "30px"}} fluid></Image>
+        <Image
+          src={icon}
+          className="mr-2"
+          style={{ height: "30px" }}
+          fluid
+        ></Image>
         <h2 className="popup-title mb-0">{title}</h2>
+        {close && (
+          <Button
+            id="close-button"
+            className="close"
+            onClick={PopupboxManager.close}
+          >
+            &times;
+          </Button>
+        )}
       </Row>
       {children}
     </Container>
@@ -15,8 +29,8 @@ export default function Popup({ title, icon, children }) {
 }
 
 // For test purposes only
-export function openPopup(title, icon) {
-  const content = <Popup title={title} icon={icon} />;
+export function openPopup(title, icon, close) {
+  const content = <Popup title={title} icon={icon} close={close}/>;
   PopupboxManager.open({
     content,
     config: {
