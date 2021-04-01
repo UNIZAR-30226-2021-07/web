a93cf100c635333a2762bca88d45367ecb121966
José Marín Díez <josemarindiez@gmail.com>
2021-04-01 17:57:07 +0200

    añadido componente chat a partida

--- src/Match.js
+++ src/Match.js
@@ -1,17 +1,22 @@
 import React from "react";
-import { Button, Container, Row } from "react-bootstrap";
+import { Button, Container, Col, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
+
+import Chat from "./Chat";
 
 function Match() {
   return (
-    <Container className="app-container">
-      <Container>
-        <Row className="align-items-center">
-          <Link to="/">
-            <Button>Salir de la partida</Button>
-          </Link>
-        </Row>
-      </Container>
+    <Container className="app-container" id="match">
+      <Row className="align-content-center">
+        <Col>
+        <Link to="/">
+          <Button>Salir de la partida</Button>
+        </Link>
+        </Col>
+        <Col>
+          <Chat/>
+        </Col>
+      </Row>
     </Container>
   );
 }
