2e2cc5184fcd6377453efc45177991de8423bc55
José Marín Díez <josemarindiez@gmail.com>
2021-04-02 20:13:59 +0200

    margen mensajes

--- src/Message.js
+++ src/Message.js
@@ -3,7 +3,7 @@ import { ListGroup } from "react-bootstrap";
 
 function Message({ message }) {
   return (
-    <ListGroup.Item className="message mt-3">
+    <ListGroup.Item className="message mt-1 mb-2">
       <strong>{message.userid}:</strong> {message.text}
     </ListGroup.Item>
   );
