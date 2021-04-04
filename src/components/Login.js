6f9ea582575f23bd950f37e4348ee598b9abcfc0
FCanfran <ferchomartin9@gmail.com>
2021-04-04 12:39:27 +0200

    intentando login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { Card, Form, Button, Container, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
-function Login({ onLogin }) {
+
+async function loginUser(credentials) {
+  return fetch('https://gatovid.herokuapp.com/data/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': 'https://gatovid.herokuapp'
+    },
+    body: JSON.stringify(credentials)
+  })
+    .then(data => data.json())
+}
+
+function Login(/*{ onLogin }*/) {
+
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    const token = await loginUser({
+      email,
+      password
+    });
+    /*setToken(token);*/
+    console.log(token)
+  }
+
   return (
     <Container className="app-container col-centered justify-content-center">
       <Card className="w-100">
@@ -12,31 +39,27 @@ function Login({ onLogin }) {
               Iniciar Sesión
             </Card.Title>
           </Row>
-          <Form className="justify-content-center">
+          <Form className="justify-content-center" onSubmit={handleSubmit}>
             <Form.Group controlId="formBasicEmail">
               <Form.Label>Email</Form.Label>
-              <Form.Control type="email" />
+              <Form.Control type="email" onChange={e => setEmail(e.target.value)} />
             </Form.Group>
 
             <Form.Group controlId="formBasicPassword">
               <Form.Label>Contraseña</Form.Label>
-              <Form.Control type="password" />
+              <Form.Control type="password" onChange={e => setPassword(e.target.value)} />
             </Form.Group>
             <Form.Group controlId="formBasicCheckbox">
               <Form.Check type="checkbox" label="Recuérdame" />
             </Form.Group>
-          </Form>
-          <Row className="justify-content-end w-100">
-            <Link to="/menu">
+            <Row className="justify-content-end w-100">
               <Button
                 className="primary-button"
-                onClick={onLogin}
-                type="submit"
-              >
+                type="submit">
                 Iniciar Sesión
               </Button>
-            </Link>
-          </Row>
+            </Row>
+          </Form>
         </Card.Body>
       </Card>
       <Container className="mt-5">
