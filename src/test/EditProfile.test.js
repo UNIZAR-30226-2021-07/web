fd84a4574eed45b27602292a092ad2ec43f95f45
FCanfran <ferchomartin9@gmail.com>
2021-04-05 12:30:18 +0200

    Revert "test run"
    This reverts commit 606b6b67e27a15ea09f8cbd18c1bfcdadbab1bfd.

--- src/test/EditProfile.test.js
+++ src/test/EditProfile.test.js
@@ -3,12 +3,29 @@ import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import EditProfile from "../components/EditProfile";
 
+import tapete from "../assets/common/boards/green.png";
+import logo from "../assets/common/logo/logo.svg";
+
 test("renders EditProfile", () => {
+  var username = "Juan Carlos";
+  var email = "juanCarlos@gmail.com";
+
   render(
     <Router>
-      <EditProfile />
+      <EditProfile
+        username={username}
+        email={email}
+        image={logo}
+        boardType={tapete}
+      />
     </Router>
   );
+
+  const usernameElement = screen.getByText(username);
+  expect(usernameElement).toBeInTheDocument();
+
+  const emailElement = screen.getByText(email);
+  expect(emailElement).toBeInTheDocument();
 
   var buttonElement = screen.getByText(/GUARDAR/i);
   expect(buttonElement).toBeInTheDocument();
