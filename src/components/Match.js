4e554b2b0c9782266cebe1d07dc43533fda3ce45
José Marín Díez <josemarindiez@gmail.com>
2021-04-16 16:27:42 +0200

    salida de partida

--- src/components/Match.js
+++ src/components/Match.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, {useContext} from "react";
 import { Button, Container, Col, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 import Chat from "./Chat";
+import { renderErrorPopup } from "./popups/ErrorPopup";
+import { SessionContext } from "./SessionProvider";
 
 function Match() {
+  const session = useContext(SessionContext);
+
+  const handleClick = () => {
+    session.socket.current.emit("leave", (response) => {
+      console.log('Adios')
+      if (response && response.error) {
+        renderErrorPopup(response.error);
+      }
+    });
+  };
   return (
     <Row className="m-0">
       <Col>
@@ -12,7 +24,7 @@ function Match() {
           <Row className="h-100">
             <Col className="md-9">
               <Link to="/">
-                <Button>Salir de la partida</Button>
+                <Button onClick={handleClick}>Salir de la partida</Button>
               </Link>
             </Col>
             <Col className="md-3"></Col>
