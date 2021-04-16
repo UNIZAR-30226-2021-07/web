cd3c00d509181e32b04f271f8089a0cec65a9188
FCanfran <ferchomartin9@gmail.com>
2021-04-16 10:59:56 +0200

    exit a match

--- src/components/Match.js
+++ src/components/Match.js
@@ -1,19 +1,34 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Button, Container, Col, Row } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
 import Chat from "./Chat";
+import { SessionContext } from "./SessionProvider";
 
 function Match() {
+  const session = useContext(SessionContext);
+  const history = useHistory();
+
+  const leaveGame = async (e) => {
+    e.preventDefault();
+      session.socket.current.emit("leave", callback);
+  };
+
+  function callback(data) {
+    if (data && data.error) {
+      console.error(data.error);
+    } else {
+      history.push("/home");
+    }
+  }
+
   return (
     <Row className="m-0">
       <Col>
         <Container className="app-container">
           <Row className="h-100">
             <Col className="md-9">
-              <Link to="/">
-                <Button>Salir de la partida</Button>
-              </Link>
+                <Button onClick={leaveGame}>Salir de la partida</Button>
             </Col>
             <Col className="md-3"></Col>
           </Row>
