de7b832e64e57d9f148915fbe79ae7331676d0fd
FCanfran <ferchomartin9@gmail.com>
2021-04-18 18:39:01 +0200

    sigo igual, propuesta de alternativas

--- src/components/popups/PreparingGamePopup.js
+++ src/components/popups/PreparingGamePopup.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useContext } from "react";
+import React, { useContext } from "react";
 import { PopupboxManager } from "react-popupbox";
 import { Row } from "react-bootstrap";
 import { useHistory } from "react-router-dom";
@@ -13,23 +13,41 @@ export default function PreparingGamePopup({ socket }) {
   const userContext = useContext(NumUsersContext);
   const total = 6;
 
-  useEffect(() => {
-    if (!socket || !socket.current) return;
+  if (!socket || !socket.current) return;
 
-    socket.current.on("start_game", (response) => {
-      if (response && response.error) {
-        renderErrorPopup(response.error);
-      } else {
-        PopupboxManager.close();
-        history.push("/match");
-      }
-    });
-
-    socket.current.on("game_owner", () => {
+  socket.current.on("start_game", (response) => {
+    if (response && response.error) {
+      renderErrorPopup(response.error);
+    } else {
       PopupboxManager.close();
-      renderStartGamePopup(socket);
-    });
+      history.push("/match");
+      // TODO: Opciones que se me ocurren:
+      // 1. Apagar la escucha temporalmente hasta cambiar el popup, y empezarla
+      //    en otro sitio con otra función asociada
+      // 2. Entender bien los componentes, y forzar un unmount de este componente
+      //    para dejar de escuchar, evitando disparos no deseados
+      // 3. Subir las escuchas a un componente más global, por encima 
+      //   (lifting-up state), intentando no hacer las escuchas desde este popup,
+      //    sino que sean más genéricas 
+      // TODO. Probando a apagar la escucha desde este popup
+      // https://stackoverflow.com/questions/59842978/is-there-any-way-to-stop-the-listening-from-socket-io
+      socket.current.off("game_owner");
+    }
   });
+
+  console.log("FASE 1")
+  
+  socket.current.on("game_owner", () => {
+    console.log("GAME_OWNER_MSG");
+    PopupboxManager.close();
+    renderStartGamePopup(socket);
+    // Return
+    return () => {
+      console.log("COMPONENT UNMOUNT 1");
+    }
+  });
+
+  console.log("FASE 2")  
 
   return (
     <Popup title="Preparando partida...">
@@ -45,6 +63,7 @@ export default function PreparingGamePopup({ socket }) {
       </Row>
     </Popup>
   );
+
 }
 
 export function renderPreparingGamePopup(socket) {
@@ -58,4 +77,5 @@ export function renderPreparingGamePopup(socket) {
       overlayClose: false,
     },
   });
+  console.log("FASE 3");
 }
