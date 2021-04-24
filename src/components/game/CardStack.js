e8642e2bcccaa277d5202b15330d0e63fe143184
José Marín Díez <josemarindiez@gmail.com>
2021-04-24 10:59:06 +0200

    animacion solamente para la mano

--- src/components/game/CardStack.js
+++ src/components/game/CardStack.js
@@ -10,7 +10,7 @@ function CardStack({ cards }) {
   return (
     <ListGroup>
       {cards.map((card, idx) => (
-        <div className="stackCard" key={idx}>
+        <div className="stack-card" key={idx}>
           <Card key={idx} number={card.number} type={card.type} />
         </div>
       ))}
