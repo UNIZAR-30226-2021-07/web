7ec032d6e024992aa0e9b99dfa7bd126dc57c47f
José Marín Díez <josemarindiez@gmail.com>
2021-04-29 20:20:38 +0200

    json and load hand

--- src/components/Match.js
+++ src/components/Match.js
@@ -15,6 +15,50 @@ import pause from "../assets/common/icons/pause.svg";
 import exit from "../assets/common/icons/logout.svg";
 import help from "../assets/common/icons/help.svg";
 
+const game_update = {
+  "finished": false,
+  "leaderboard": {
+      "manolo22": {
+          "position": 1,
+          "coins": 50,
+      },
+  },
+  "playtime_mins": 4,
+  "current_turn": "manolo22",
+  "players": [
+      {
+          "name": "marcuspkz",
+          "picture": 4,
+          "board": 2,
+      },
+      // ...
+  ],
+  "hand": [
+      {"card_type": "organ", "color": "red"},
+      {"card_type": "virus", "color": "green"},
+      {"card_type": "treatment", "treatment_type": "infection"},
+  ],
+  "bodies": {
+      "marcuspkz": [
+          {
+              "organ": {
+                  "card_type": "organ",
+                  "color": "red"
+              },
+              "modifiers": [
+                  {"card_type": "virus", "color": "red"},
+              ]
+          },
+      ],
+  },
+};
+
+const loadHand = (game_update) => {
+  console.log('Load hand');
+  let hand = game_update.hand[0].card_type
+  return hand;
+}
+
 function Match() {
   const session = useContext(SessionContext);
   const history = useHistory();
