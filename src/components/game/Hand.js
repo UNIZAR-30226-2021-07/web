9d69efa839773a1f16d3d216984287b74af4e905
José Marín Díez <josemarindiez@gmail.com>
2021-05-04 09:27:36 +0200

    Mano

--- src/components/game/Hand.js
+++ src/components/game/Hand.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, useContext} from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { ListGroup } from "react-bootstrap";
 
 import Card from "./Card";
