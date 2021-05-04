bdccea6445886221143f0ff0f0f97228a4a98139
José Marín Díez <josemarindiez@gmail.com>
2021-05-04 09:38:33 +0200

    Estructura cuerpo

--- src/components/Board.js
+++ src/components/Board.js
@@ -16,16 +16,11 @@ function Board() {
     setNumRivals(game.players.length);
   }, [game.players]);
 
-  const cardsStack = [
-    { card_type: "organ", color: "blue" },
-    { card_type: "virus", color: "green" },
-  ];
   const cardsStack1 = [
     { card_type: "organ", color: "yellow" },
     { card_type: "organ", color: "blue" },
   ];
 
-  const cardBody = [cardsStack, cardsStack, cardsStack, cardsStack];
   const body = [cardsStack1, cardsStack1, cardsStack1, cardsStack1];
 
   const Player1 = () => {
@@ -70,7 +65,7 @@ function Board() {
         </Col>
       </Row>
       <Row className="justify-content-center">
-        <Body cardStacks={cardBody} />
+        <Body />
       </Row>
       <Row className="justify-content-center">
         <Hand />
--- src/components/game/Body.js
+++ src/components/game/Body.js
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useContext, useState, useEffect } from "react";
 
 import CardStack from "./CardStack";
+import { GameContext } from "../GameProvider";
 
 /**
  * @param cardStacks -> son las pilas de cartas posibles en el cuerpo
  * Entre 0 y 4 pilas
  */
-function Body({ cardStacks, kind = "body" }) {
+function Body({ kind = "body" }) {
+  const game = useContext(GameContext);
+  const [body, setBody] = useState([]);
+
+  useEffect(() => {
+    setBody(game.body);
+  }, [game.body]);
   return (
     <div className="row mx-0 flex-nowrap" id={`user-${kind}`}>
-      {cardStacks.map((cards, idx) => (
+      {body.map((cards, idx) => (
         <CardStack
           id={`${kind}-stack-${idx}`}
           key={idx}
