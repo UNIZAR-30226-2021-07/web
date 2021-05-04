057bfe873ab7498af88ae6ae47e05eeb51336106
José Marín Díez <josemarindiez@gmail.com>
2021-05-04 13:21:49 +0200

    Fix getCard

--- src/utils/json.js
+++ src/utils/json.js
@@ -1,13 +1,20 @@
 import cards from "../assets/common/cards.json";
 import profile_pics from "../assets/common/profile_pics.json";
 
+function searchCard(elem, card) {
+  if (card.color) {
+    return elem.type == card.card_type && elem.color == card.color;
+  } else {
+    return (
+      elem.type == card.card_type && elem.treatment_type == card.treatment_type
+    );
+  }
+}
+
 // Create card url
 export function getCard(card) {
-  let carta = cards.find((elto) =>
-    elto.type == card.card_type && card.color
-      ? elto.color == card.color
-      : elto.treatment_type == card.treatment_type
-  );
+
+  let carta = cards.find((elem) => searchCard(elem, card));
 
   return process.env.PUBLIC_URL + "/" + carta.image;
 }
