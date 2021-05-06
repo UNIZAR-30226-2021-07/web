d3c4934ea2fb3327564816aa8f9c2e5f5e6c2566
José Marín Díez <josemarindiez@gmail.com>
2021-05-06 16:15:36 +0200

    Confirmar compra popup

---
+++ src/components/popups/ConfirmPurchasePopup.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { PopupboxManager } from "react-popupbox";
+import { Row, Button, Image } from "react-bootstrap";
+
+import Popup from "./PopUp";
+
+import coins from "../../assets/common/icons/huella.svg";
+
+export default function ConfirmPurchasePopup({ price }) {
+  return (
+    <Popup title="¿Quiere comprar el artículo?" close={true}>
+      <Row className="coins shop justify-content-center align-items-center">
+        <span className="mr-2">
+          {price}
+        </span>
+        <Image src={coins} alt="Precio" />
+      </Row>
+      <Row>
+        <Button
+          className="success-button mb-3 mt-4"
+          style={{ width: "100%" }}
+          onClick={PopupboxManager.close}
+        >
+          Sí
+        </Button>
+      </Row>
+      <Row>
+        <Button
+          className="alert-button"
+          style={{ width: "100%" }}
+          onClick={PopupboxManager.close}
+        >
+          No
+        </Button>
+      </Row>
+    </Popup>
+  );
+}
+
+export function renderConfirmPurchasePopup(price) {
+  const content = <ConfirmPurchasePopup price={price} />;
+  PopupboxManager.open({
+    content,
+    config: {
+      fadeIn: true,
+      fadeInSpeed: 400,
+      fadeOut: true,
+      fadeOutSpeed: 50,
+      escClose: false,
+      overlayClose: false,
+    },
+  });
+}
