e132047614e23242a8b2482a6501e38662386ef5
José Marín Díez <josemarindiez@gmail.com>
2021-05-18 11:25:48 +0200

    Estilo a partir de diccionario

--- src/components/popups/LeaderboardPopup.js
+++ src/components/popups/LeaderboardPopup.js
@@ -10,20 +10,36 @@ import dinero from "../../assets/common/icons/huella.svg";
 
 import Popup from "./PopUp";
 
-function Player() {
-  return (
-    <Row className="justify-content-between align-items-center my-2 coins">
-      <Image src={diploma} style={{ height: "30px" }} />
-      <h5 className="font-weight-bold mx-3 my-0">Jugador 6</h5>
-      <h5 className="font-weight-bold my-0">
-        10
-        <Image className="ml-2" src={dinero} />
-      </h5>
-    </Row>
-  );
-}
-
 export default function LeaderboardPopup() {
+  const leaderboard = {
+    manolo22: {
+      position: 1,
+      coins: 60,
+    },
+    juan: {
+      position: 2,
+      coins: 50,
+    },
+    pedro: {
+      position: 3,
+      coins: 40,
+    },
+    daniel: {
+      position: 4,
+      coins: 30,
+    },
+    jaime: {
+      position: 5,
+      coins: 20,
+    },
+    carmen: {
+      position: 6,
+      coins: 10,
+    },
+  };
+
+  let players = Object.keys(leaderboard);
+
   const handleClick = (e) => {
     e.preventDefault();
     PopupboxManager.close();
@@ -31,47 +47,35 @@ export default function LeaderboardPopup() {
 
   return (
     <Popup title="Resultados" close={false}>
-      <Row className="justify-content-between align-items-center my-2 coins ">
-        <Image src={oro} style={{ height: "30px" }} />
-        <h5 className="font-weight-bold mx-3 my-0">Jugador 1</h5>
-        <h5 className="font-weight-bold my-0">
-          50
-          <Image className="ml-2" src={dinero} />
-        </h5>
-      </Row>
-      <Row className="justify-content-between align-items-center my-2 coins">
-        <Image src={plata} style={{ height: "30px" }} />
-        <h5 className="font-weight-bold mx-3 my-0">Jugador 2</h5>
-        <h5 className="font-weight-bold my-0">
-          30
-          <Image className="ml-2" src={dinero} />
-        </h5>
-      </Row>
-      <Row className="justify-content-between align-items-center my-2 coins">
-        <Image src={bronce} style={{ height: "30px" }} />
-        <h5 className="font-weight-bold mx-3 my-0">Jugador 3</h5>
-        <h5 className="font-weight-bold my-0">
-          30
-          <Image className="ml-2" src={dinero} />
-        </h5>
-      </Row>
-      <Row className="justify-content-between align-items-center my-2 coins">
-        <Image src={diploma} style={{ height: "30px" }} />
-        <h5 className="font-weight-bold mx-3 my-0">Jugador 4</h5>
-        <h5 className="font-weight-bold my-0">
-          10
-          <Image className="ml-2" src={dinero} />
-        </h5>
-      </Row>
-      <Row className="justify-content-between align-items-center my-2 coins">
-        <Image src={diploma} style={{ height: "30px" }} />
-        <h5 className="font-weight-bold mx-3 my-0">Jugador 5</h5>
-        <h5 className="font-weight-bold my-0">
-          10
-          <Image className="ml-2" src={dinero} />
-        </h5>
-      </Row>
-      <Player>
+      {players.map((player, idx) => {
+        let position = leaderboard[player].position;
+        let coins = leaderboard[player].coins;
+
+        return (
+          <Row
+            key={idx}
+            className="justify-content-between align-items-center my-2 coins"
+          >
+            <Image
+              src={
+                position == 1
+                  ? oro
+                  : position == 2
+                  ? plata
+                  : position == 3
+                  ? bronce
+                  : diploma
+              }
+              style={{ height: "30px" }}
+            />
+            <h5 className="font-weight-bold mx-3 my-0">{player}</h5>
+            <h5 className="font-weight-bold my-0">
+              {coins}
+              <Image className="ml-2" src={dinero} />
+            </h5>
+          </Row>
+        );
+      })}
       <Row>
         <Button className="primary-button w-100 mt-2" onClick={handleClick}>
           Cerrar
