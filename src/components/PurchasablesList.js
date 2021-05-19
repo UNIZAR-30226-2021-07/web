c5d59be1679eb3f7de3622226e724e12fc736afe
José Marín Díez <josemarindiez@gmail.com>
2021-05-19 19:57:58 +0200

    Cruces shop

--- src/components/PurchasablesList.js
+++ src/components/PurchasablesList.js
@@ -1,5 +1,7 @@
 import React from "react";
-import { Card, Row, Col } from "react-bootstrap";
+import { Card, Row, Col, Button } from "react-bootstrap";
+
+import { useHistory } from "react-router-dom";
 
 import PurchasableBox from "./PurchasableBox";
 
@@ -7,13 +9,21 @@ import boards from "../assets/common/boards.json";
 import profile_pics from "../assets/common/profile_pics.json";
 
 function PurchasablesList({ title, type }) {
+  const history = useHistory();
   let elements = type == "board" ? boards : profile_pics;
   // Limit profile_pics to show, all except last: reserved for IA
   const elemsLength = elements.length;
   const rangeIA = type == "board" ? false : true;
 
+  const handleClose = () => {
+    history.push("/menu");
+  };
+
   return (
     <Card className="shop-card">
+      <Button className="close card-close" onClick={handleClose}>
+        &times;
+      </Button>
       <Card.Body>
         <Row className="justify-content-center align-items-center ">
           <Card.Title className="primary-title text-align-center">
